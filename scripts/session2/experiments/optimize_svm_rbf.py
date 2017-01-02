8931b60fe28bb116e00ae37ca688711b4bc60613
ArantxaCasanova <ar.casanova.8@gmail.com>
2017-01-02 11:59:22 +0100

    Added plot in rbf optimization

--- scripts/session2/experiments/optimize_svm_rbf.py
+++ scripts/session2/experiments/optimize_svm_rbf.py
@@ -1,7 +1,11 @@
 from __future__ import print_function, division
 
 import time
+import argparse
+import itertools
 
+
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.svm import SVC
@@ -14,8 +18,7 @@ import mlcv.input_output as io
 """ CONSTANTS """
 N_JOBS = 8
 
-""" MAIN SCRIPT"""
-if __name__ == '__main__':
+def train():
     start = time.time()
 
     # Read the training set
@@ -41,8 +44,8 @@ if __name__ == '__main__':
     print('\nSTARTING HYPERPARAMETER OPTIMIZATION FOR RBF SVM')
     codebook_k_values = [2 ** i for i in range(7, 16)]
     params_distribution = {
-        'C': np.logspace(-1, 3, 10 ** 6),
-        'gamma' : np.logspace(0, 2, 10 ** 6)
+        'C': np.logspace(-4, 3, 10 ** 6),
+        'gamma' : np.logspace(-3, 5, 10 ** 6)
     }
     n_iter = 100
     best_accuracy = 0
@@ -79,7 +82,7 @@ if __name__ == '__main__':
 
         # Appending all parameter-scores combinations
         cv_results.update({k: random_search.cv_results_})
-        io.save_object(cv_results, 'lrbf_svm_optimization')
+        io.save_object(cv_results, 'rbf_svm_optimization')
 
         # Obtaining the parameters which yielded the best accuracy
         if random_search.best_score_ > best_accuracy:
@@ -90,8 +93,46 @@ if __name__ == '__main__':
         print('-------------------------------\n')
 
     print('\nBEST PARAMS')
-    print('k={}, C={} --> accuracy: {:.3f}'.format(best_params['k'], best_params['C'], best_accuracy))
+    print('k={}, C={} , gamma={} --> accuracy: {:.3f}'.format(best_params['k'], best_params['C'],best_params['gamma'], best_accuracy))
 
     print('Saving all cross-validation values...')
     io.save_object(cv_results, 'rbf_svm_optimization')
     print('Done')
+
+def plot_curve():
+    res = io.load_object('rbf_svm_optimization')
+    colors = itertools.cycle(
+        ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'darkolivegreen', 'darkviolet', 'black']
+    )
+    plt.figure()
+    for k in res:
+        results = res[k]
+        x = results['param_C']
+        y = results['mean_test_score']
+        e = results['std_test_score']
+        sorted_indices = x.argsort()
+        x_sorted = np.asarray(x[sorted_indices], dtype=np.float64)
+        y_sorted = np.asarray(y[sorted_indices], dtype=np.float64)
+        e_sorted = np.asarray(e[sorted_indices], dtype=np.float64)
+        color = colors.next()
+        plt.errorbar(x_sorted, y_sorted, e_sorted, label='{} visual words'.format(k), color=color)
+
+    plt.legend()
+    plt.title('Optimization of C for Linear SVM')
+    plt.xlabel('C')
+    plt.ylabel('Accuracy')
+    plt.show()
+
+
+""" MAIN SCRIPT"""
+if __name__ == '__main__':
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('type', default='plot', choices=['train', 'plot'])
+    args = args_parser.parse_args()
+    exec_option = args.type
+
+    if exec_option == 'train':
+        train()
+    else:
+        plot_curve()
+    exit(0)
