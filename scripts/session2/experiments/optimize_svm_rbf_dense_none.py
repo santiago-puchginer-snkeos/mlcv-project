5eeb99cb8c4a0fde3c5f413ca992dfdf737c84f6
Anna Martí <annamartiaguilera@gmail.com>
2017-01-04 19:44:50 +0100

    optimize rbf with dense sift

---
+++ scripts/session2/experiments/optimize_svm_rbf_dense_none.py
@@ -0,0 +1,155 @@
+from __future__ import print_function, division
+
+import argparse
+import itertools
+import time
+
+from mpl_toolkits.mplot3d import Axes3D
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+import mlcv.bovw as bovw
+import mlcv.feature_extraction as feature_extraction
+import mlcv.inpxut_output as io
+
+""" CONSTANTS """
+N_JOBS = 8
+
+
+def train():
+    start = time.time()
+
+    # Read the training set
+    train_images_filenames, train_labels = io.load_training_set()
+    print('Loaded {} train images.'.format(len(train_images_filenames)))
+
+    # Feature extraction with sift
+    print('Obtaining dense sift features...')
+    try:
+        D, L, I = io.load_object('train_dense_descriptors', ignore=True), \
+                  io.load_object('train_dense_labels', ignore=True), \
+                  io.load_object('train_dense_indices', ignore=True)
+    except IOError:
+        D, L, I = feature_extraction.parallel_dense(train_images_filenames, train_labels, num_samples_class=-1,
+                                                   n_jobs=N_JOBS)
+        io.save_object(D, 'train_dense_descriptors', ignore=True)
+        io.save_object(L, 'train_dense_labels', ignore=True)
+        io.save_object(I, 'train_dense_indices', ignore=True)
+    print('Time spend: {:.2f} s'.format(time.time() - start))
+
+    # Start hyperparameters optimization
+    print('\nSTARTING HYPERPARAMETER OPTIMIZATION FOR RBF SVM')
+    codebook_k_values = [2 ** i for i in range(7, 16)]
+    params_distribution = {
+        'C': np.logspace(-4, 1, 10 ** 3),
+        'gamma': np.logspace(-3, 1, 10 ** 3)
+    }
+    n_iter = 100
+    best_accuracy = 0
+    best_params = {}
+    cv_results = {}
+
+    # Iterate codebook values
+    for k in codebook_k_values:
+        temp = time.time()
+        print('Creating codebook with {} visual words'.format(k))
+        D = D.astype(np.uint32)
+        codebook = bovw.create_codebook(D, k=k, codebook_name='codebook_{}_dense'.format(k))
+        print('Time spend: {:.2f} s'.format(time.time() - temp))
+        temp = time.time()
+
+        print('Getting visual words from training set...')
+        vis_words, labels = bovw.visual_words(D, L, I, codebook,normalization=None)
+        print('Time spend: {:.2f} s'.format(time.time() - temp))
+        temp = time.time()
+
+        print('Scaling features...')
+        std_scaler = StandardScaler().fit(vis_words)
+        vis_words = std_scaler.transform(vis_words)
+        print('Time spend: {:.2f} s'.format(time.time() - temp))
+        temp = time.time()
+
+        print('Optimizing SVM hyperparameters...')
+        svm = SVC(kernel='rbf')
+        random_search = RandomizedSearchCV(svm, params_distribution, n_iter=n_iter, scoring='accuracy', n_jobs=N_JOBS,
+                                           refit=False, verbose=1, cv=4)
+        random_search.fit(vis_words, labels)
+        print('Time spend: {:.2f} s'.format(time.time() - temp))
+
+        # Convert MaskedArrays to ndarrays to avoid unpickling bugs
+        results = random_search.cv_results_
+        results['param_C'] = results['param_C'].data
+        results['param_gamma'] = results['param_gamma'].data
+
+        # Appending all parameter-scores combinations
+        cv_results.update({k: results})
+        io.save_object(cv_results, 'rbf_svm_optimization_dense_none')
+
+        # Obtaining the parameters which yielded the best accuracy
+        if random_search.best_score_ > best_accuracy:
+            best_accuracy = random_search.best_score_
+            best_params = random_search.best_params_
+            best_params.update({'k': k})
+
+        print('-------------------------------\n')
+
+    print('\nBEST PARAMS')
+    print('k={}, C={} , gamma={} --> accuracy: {:.3f}'.format(best_params['k'], best_params['C'], best_params['gamma'],
+                                                              best_accuracy))
+
+    print('Saving all cross-validation values...')
+    io.save_object(cv_results, 'rbf_svm_optimization_dense_none')
+    print('Done')
+
+
+def plot_curve():
+    print('Loading results object...')
+    res = io.load_object('rbf_svm_optimization_dense_none', ignore=True)
+
+    print('Plotting...')
+    colors = itertools.cycle(
+        ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'darkolivegreen', 'darkviolet', 'black']
+    )
+    fig = plt.figure(figsize=(40, 20), facecolor='white')
+    # Compute subplot parameters
+    num_subplots = len(res)
+    num_rows = np.ceil(num_subplots / 2)
+    # All subplots
+    for ind, k in enumerate(sorted(res.keys())):
+        results = res[k]
+        x = results['param_C']
+        y = results['param_gamma']
+        z = results['mean_test_score']
+        print('For codebook with visual words {} '.format(k))
+        print('Accuracy {}'.format(max(z)))
+        print ('C {}'.format(x[np.argmax(z)]))
+        print('gamma {}'.format(y[np.argmax(z)]))
+
+        color = colors.next()
+        ax = fig.add_subplot(num_rows, 2, ind + 1, projection='3d')
+        ax.scatter(x, y, z, c=color, lw=2)
+        ax.set_title('{} visual words'.format(k))
+        ax.set_xlabel('C')
+        ax.set_ylabel('gamma')
+        ax.set_zlabel('Accuracy')
+    plt.tight_layout()
+    plt.show()
+    plt.close()
+
+
+""" MAIN SCRIPT"""
+if __name__ == '__main__':
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--type', default='train', choices=['train', 'plot'])
+    args = args_parser.parse_args()
+    exec_option = args.type
+
+    if exec_option == 'train':
+        train()
+    else:
+        plot_curve()
+    exit(0)
