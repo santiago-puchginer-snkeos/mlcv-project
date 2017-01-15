0fbf4dcfe65d35b029e343cb7a6d72a678c40b09
Anna Martí <annamartiaguilera@gmail.com>
2017-01-15 21:13:55 +0100

    dense sampling_density optimization changed

--- scripts/session3/experiments/optimize_fisher_vectors_power_session3.py
+++ scripts/session3/experiments/optimize_fisher_vectors_power_session3.py
@@ -18,7 +18,7 @@ import mlcv.kernels as kernels
 import mlcv.settings as settings
 
 """ PARAMETER SWEEP """
-dense_sampling_density = [2, 4, 8, 16]
+dense_sampling_density = [8, 16]
 codebook_size = [16, 32, 64]
 pca_reduction = [60, 80, 100, 120]
 params_distribution = {
